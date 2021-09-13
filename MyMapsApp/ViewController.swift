3b2384c9a5d9025a1c4cdaa9d8f2c5d2dcab8fb3
Majm1k <ru.zaykin@mail.ru>
2021-09-13 22:04:36 +0300

    Added control buttons + design, imported MapKit and CoreLocation

--- MyMapsApp/ViewController.swift
+++ MyMapsApp/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    //MARK: - PROPERTIES
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var routeButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    //MARK: - VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.addButton.layer.cornerRadius = 10
+        self.routeButton.layer.cornerRadius = 10
+        self.resetButton.layer.cornerRadius = 10
     }
 
-
+    //MARK: - BUTTON ACTION
+    @IBAction func addButtonAction(_ sender: UIButton) {
+        print("Добавить")
+    }
+    
+    @IBAction func routeButtonAction(_ sender: UIButton) {
+        print("Построить")
+    }
+    
+    @IBAction func resetButtonAction(_ sender: Any) {
+        print("Сбросить")
+    }
 }
 
